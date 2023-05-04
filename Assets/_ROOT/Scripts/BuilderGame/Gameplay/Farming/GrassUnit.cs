f54ebff820ab56c48c3f6e4bef3075ad18fe0bac
Olesia Pashko <pashkostela@gmail.com>
2023-05-04 14:04:58 +0300

    add layers, rigidbody, colliders and basic destroy logic

--- Assets/_ROOT/Scripts/BuilderGame/Gameplay/Farming/GrassUnit.cs
+++ Assets/_ROOT/Scripts/BuilderGame/Gameplay/Farming/GrassUnit.cs
@@ -1,9 +1,15 @@
 namespace BuilderGame.Gameplay.Farming
 {
+    using System;
     using UnityEngine;
 
     public class GrassUnit : MonoBehaviour
     {
         public float Size => 1f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Destroy(gameObject);
+        }
     }
 }
